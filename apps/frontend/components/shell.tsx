e79b3192cac40f3956a03af6ff16b010d1674d24
d-ivashchuk <dimaiv@me.com>
2023-06-22 14:04:46 +0200

    hide navbar on click

--- apps/frontend/components/shell.tsx
+++ apps/frontend/components/shell.tsx
@@ -19,18 +19,32 @@ import {
   Title,
   LoadingOverlay,
   Stack,
+  Space,
+  Flex,
+  ActionIcon,
 } from '@mantine/core';
+import { matches } from '@mantine/form';
+import { useMediaQuery } from '@mantine/hooks';
 import { Notifications } from '@mantine/notifications';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode, useState } from 'react';
 
-import { FaClock, FaCheckSquare, FaHammer, FaFolder } from 'react-icons/fa';
+import {
+  FaClock,
+  FaCheckSquare,
+  FaHammer,
+  FaFolder,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+} from 'react-icons/fa';
 
 export default function Page({ children }: { children: ReactNode }) {
   const theme = useMantineTheme();
   const { user, isLoaded } = useUser();
   const [opened, setOpened] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
+  const matches = useMediaQuery('(max-width: 767px)');
 
   if (!isLoaded)
     return (
@@ -59,42 +73,73 @@ export default function Page({ children }: { children: ReactNode }) {
                 : theme.colors.gray[0],
           },
         }}
-        navbarOffsetBreakpoint={user ? 'sm' : 0}
-        asideOffsetBreakpoint="sm"
+        navbarOffsetBreakpoint={user ? (collapsed ? '70px' : '200px') : 0}
+        asideOffsetBreakpoint="760px"
         navbar={
           user ? (
             <Navbar
               p="xs"
               hiddenBreakpoint="sm"
               hidden={!opened}
-              width={{ sm: 200, lg: 300 }}
+              style={{
+                transition: 'width 0.2s ease',
+              }}
+              width={{ sm: collapsed ? '70px' : '200px' }}
             >
-              <Group spacing={1} px={20}>
-                <NavbarItem
-                  href="jobs"
-                  icon={<FaClock />}
-                  label="Jobs"
-                  color="green"
-                />
-                <NavbarItem
-                  href="runs"
-                  icon={<FaCheckSquare />}
-                  label="Runs"
-                  color="violet"
-                />
-                <NavbarItem
-                  href="projects"
-                  icon={<FaFolder />}
-                  label="Projects"
-                  color="yellow"
-                />
-                <NavbarItem
-                  href="settings"
-                  icon={<FaHammer />}
-                  label="Settings"
-                  color="blue"
-                />
-              </Group>
+              <Flex
+                style={{ height: '100%' }}
+                direction="column"
+                px={0}
+                justify="space-between"
+              >
+                <Box>
+                  <NavbarItem
+                    href="jobs"
+                    icon={<FaClock />}
+                    label="Jobs"
+                    color="green"
+                    collapsed={collapsed}
+                  />
+                  <NavbarItem
+                    href="runs"
+                    icon={<FaCheckSquare />}
+                    label="Runs"
+                    color="violet"
+                    collapsed={collapsed}
+                  />
+                  <NavbarItem
+                    href="projects"
+                    icon={<FaFolder />}
+                    label="Projects"
+                    color="yellow"
+                    collapsed={collapsed}
+                  />
+                  <NavbarItem
+                    href="settings"
+                    icon={<FaHammer />}
+                    label="Settings"
+                    color="blue"
+                    collapsed={collapsed}
+                  />
+                </Box>
+                <Box
+                  style={{
+                    padding: '0.5rem',
+                  }}
+                >
+                  <ActionIcon
+                    onClick={() => setCollapsed((c) => !c)}
+                    variant="filled"
+                    color="gray"
+                  >
+                    {collapsed ? (
+                      <FaArrowAltCircleRight size="1rem" />
+                    ) : (
+                      <FaArrowAltCircleLeft size="1rem" />
+                    )}
+                  </ActionIcon>
+                </Box>
+              </Flex>
             </Navbar>
           ) : undefined
         }
@@ -106,29 +151,37 @@ export default function Page({ children }: { children: ReactNode }) {
         header={
           <Header height={{ base: 50, md: 70 }} p="md">
             <div
-              style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                height: '100%',
+              }}
             >
-              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-                <Burger
-                  opened={opened}
-                  onClick={() => setOpened((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
-
-              <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+              {matches ? (
+                <>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
+                  />
+                  <Box>
+                    <Image src="/logo.png" width="50" height="50" alt="logo" />
+                  </Box>
+                </>
+              ) : (
                 <Group w="100%" position="apart">
                   <Group>
-                    <Image src="/logo.png" width="70" height="70" alt="logo" />
-                    <Text weight="500" size="xl">
+                    <Image src="/logo.png" width="50" height="50" alt="logo" />
+                    <Text weight="500" size="xl" pos="relative" left="-15px">
                       Nightscan
                     </Text>
                   </Group>
                   <UserButton />
                 </Group>
-              </MediaQuery>
+              )}
             </div>
           </Header>
         }
@@ -144,11 +197,13 @@ const NavbarItem = ({
   label,
   color,
   href,
+  collapsed,
 }: {
   icon: ReactNode;
   label: string;
   color: string;
   href: string;
+  collapsed: boolean;
 }) => {
   return (
     <Box w="100%">
@@ -176,13 +231,19 @@ const NavbarItem = ({
             },
           })}
         >
-          <Group>
+          {collapsed ? (
             <ThemeIcon color={color} variant="light">
               {icon}
             </ThemeIcon>
+          ) : (
+            <Group>
+              <ThemeIcon color={color} variant="light">
+                {icon}
+              </ThemeIcon>
 
-            <Text size="sm">{label}</Text>
-          </Group>
+              <Text size="sm">{label}</Text>
+            </Group>
+          )}
         </UnstyledButton>
       </Link>
     </Box>
